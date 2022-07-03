11fb93ebd68c0a0a397edc1f294c59719efc663e
vjr1990 <jagadeeswara.vangala@accenture.com>
2022-07-03 22:07:09 +0530

    updated version

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+      required_providers {
+         aws = {
+         source = "hashicorp/aws"
+         version = "= 3.74.2"
+        }
+     }
+  }
 provider "aws" {
    profile    = "dev"
    region     = var.region
