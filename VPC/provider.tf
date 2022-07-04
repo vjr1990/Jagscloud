9a7fb29fbf39b494daa650c2a7282a9c9ab161a7
vjr1990 <35777558+vjr1990@users.noreply.github.com>
2022-07-04 12:27:02 +0530

    Update provider.tf

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-   region = "us-east-1"
-   profile = "dev"
-   shared_credentials_file = "/root/.aws/credentials"
+  shared_credentials_file = "/root/.aws/credentials"
+  profile                 = "dev"
+  region                  = "us-east-1"
 }
-
